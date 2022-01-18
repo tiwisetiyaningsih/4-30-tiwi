181df2e49544a390c1d5b0a2329052774311d819
tiwisetiyaningsih <tiwisetiyaningsih@gmail.com>
2022-01-18 22:01:24 +0700

    Laravel_2

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Tiwi Arum Setyaningsih" ,
         "email" => "3103120222@student.smktelkom-pwt.sch.id" ,
         "gambar" => "tiwi.png"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
