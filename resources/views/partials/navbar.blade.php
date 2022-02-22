91b40f899166d867123ce416259e021a56c12fd1
tiwisetiyaningsih <tiwisetiyaningsih@gmail.com>
2022-02-22 15:14:20 +0700

    upload modul 6

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
     <a class="navbar-brand" href="/">Tiwi</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +23,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route ('login')}}">Login</a>
             </li>
-            <li class="nav-item">
+            <li c lass="nav-item">
                 <a class="nav-link" href="{{route ('register')}}">Register</a>
             </li>
         </ul>
