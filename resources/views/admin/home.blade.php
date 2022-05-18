8d7ad307dd72fce7768113152c776bc032676dd5
tiwisetiyaningsih <tiwisetiyaningsih@gmail.com>
2022-05-19 00:26:57 +0700

    hosting1

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,6 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="overview-wrap">
-                                    <h2 class="title-1">overview</h2>
                                 </div>
                             </div>
                         </div>
